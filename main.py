f09cc30d7e651608c7912f3a10c263024f969639
IsaacRh <isaac.roj_12@hotmail.com>
2021-02-28 18:56:53 -0600

    [ADD] -- use request and make_response

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
+    response = make_response(redirect('/hi'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/hi')
+def hello():
+    user_ip = request.cookies.get('user_ip')
     return 'Hellor world from flask your ip is {}'.format(user_ip)

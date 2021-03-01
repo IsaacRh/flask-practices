898a1f74616e9795c095bb30e83a46da70a55f1f
IsaacRh <isaac.roj_12@hotmail.com>
2021-02-28 18:48:26 -0600

    [ADD] -- set ip from user in response

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hellor world from flask'
+    user_ip = request.remote_addr
+    return 'Hellor world from flask your ip is {}'.format(user_ip)

a981fd1f5154f955a2a2f48ebd84c2fa71eddc30
IsaacRh <isaac.roj_12@hotmail.com>
2021-03-03 09:57:35 -0600

    [ADD] --Flask-WTF implementation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -9,6 +12,13 @@ app.config['SECRET_KEY'] = 'secret'
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
+# CLASS
+
+class LoginForm(FlaskForm):
+    user_name = StringField('User name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit_button = SubmitField('Send', validators=[DataRequired()])
+
 # ERRORS
 
 @app.errorhandler(404)
@@ -16,6 +26,7 @@ def not_found(error):
     return render_template('404.html', error=error)
 
 
+# RUOUTES
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -27,10 +38,12 @@ def index():
 @app.route('/hi')
 def hello():
     #user_ip = request.cookies.get('user_ip')
+    login_form = LoginForm()
     user_ip = session.get('user_ip')
     context = {
         'user_ip' : user_ip,
         'todos': todos,
+        'login_form' : login_form,
     }
     # double asteric expand the context
     return render_template('hello.html', **context)
